78e636d915c676a56e6b8a5dcf75859bc5a300c8
Lauchi <simonheiss87@gmail.com>
2018-03-02 18:41:20 +0100

    Add project files.

---
+++ GenericWebServiceBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericWebServiceBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
