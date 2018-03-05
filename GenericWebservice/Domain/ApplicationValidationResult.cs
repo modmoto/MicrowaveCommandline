c42305c90ac8eb1ef636ebb6d20a501057c240bc
Lauchi <simonheiss87@gmail.com>
2018-03-05 23:22:05 +0100

    rename

---
+++ GenericWebservice/Domain/ApplicationValidationResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Domain
+{
+    public class ApplicationValidationResult
+    {
+        private ApplicationValidationResult(bool ok, IEnumerable<string> errors)
+        {
+            Ok = ok;
+            Errors = errors;
+        }
+
+        public static ApplicationValidationResult OkResult()
+        {
+            return new ApplicationValidationResult(true, new List<string>());
+        }
+
+        public static ApplicationValidationResult ErrorResult(IList<string> errors)
+        {
+            return new ApplicationValidationResult(false, errors);
+        }
+
+        public bool Ok { get; }
+        public IEnumerable<string> Errors { get; }
+    }
+}
