b6837d30341d472d56e4087537050722ea2fcb34
Simon Heiss <simonheiss87@gmail.com>
2018-03-12 09:04:36 +0100

    added tests for repo interface

---
+++ DslModelToCSharp.Tests/Application/RepositoryBuilderTests.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using DslModelToCSharp.Application;
+using FileToDslModel;
+using FileToDslModel.Lexer;
+using FileToDslModel.ParseAutomat;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DslModelToCSharp.Tests.Application
+{
+    [TestClass]
+    public class RepositoryBuilderTests : TestBase
+    {
+        [TestMethod]
+        public void Build()
+        {
+            var commandHandlerBuilder = new IRepositoryBuilder(ApplicationNameSpace);
+
+            using (var reader = new StreamReader("Schema.wsb"))
+            {
+                var content = reader.ReadToEnd();
+                var domainTree = new DslParser(new Tokenizer(), new Parser()).Parse(content);
+                foreach (var domainClass in domainTree.Classes)
+                {
+                    var codeNamespace = commandHandlerBuilder.Build(domainClass);
+                    new FileWriter(BasePathApplication).WriteToFile(codeNamespace.Types[0].Name, domainClass.Name + "s", codeNamespace);
+                }
+            }
+
+            new PrivateSetPropertyHackCleaner().ReplaceHackPropertyNames(BasePathApplication);
+
+            Assert.AreEqual(File.ReadAllText("../../../ApplicationExpected/Generated/Posts/IPostRepository.g.cs"),
+                File.ReadAllText("Application/Posts/IPostRepository.g.cs"));
+            Assert.AreEqual(File.ReadAllText("../../../ApplicationExpected/Generated/Users/IUserRepository.g.cs"),
+                File.ReadAllText("Application/Users/IUserRepository.g.cs"));
+        }
+    }
+}
