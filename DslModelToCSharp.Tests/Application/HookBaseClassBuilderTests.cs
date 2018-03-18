8bbd3aaf51f680fe553a7675e6c1bad48cedb311
Lauchi <simonheiss87@gmail.com>
2018-03-18 10:04:28 +0100

    added test for domainhook interface

---
+++ DslModelToCSharp.Tests/Application/HookBaseClassBuilderTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using DslModel.Application;
+using DslModelToCSharp.Application;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DslModelToCSharp.Tests.Application
+{
+    [TestClass]
+    public class HookBaseClassBuilderTests : TestBase
+    {
+        [TestMethod]
+        public void Write()
+        {
+            var hookResultBuilder = new HookBaseClassBuilder(ApplicationNameSpace);
+
+            var codeNamespace = hookResultBuilder.Build(new DomainHookBaseClass());
+
+            new FileWriter(BasePathApplication).WriteToFile(codeNamespace.Types[0].Name, "Base", codeNamespace);
+
+            new PrivateSetPropertyHackCleaner().ReplaceHackPropertyNames(BasePathApplication);
+
+            Assert.AreEqual(File.ReadAllText("../../../ApplicationExpected/Generated/Base/IDomainHook.g.cs"),
+                File.ReadAllText("Application/Base/IDomainHook.g.cs"));
+        }
+    }
+}
