80c1f38720b615f3a25afbd8555f2b0efaff66a3
Lauchi <simonheiss87@gmail.com>
2018-03-18 10:13:37 +0100

    added dep inj tests

---
+++ DslModelToCSharp.Tests/DependencyInjectionWriterTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using DslModel.Application;
+using DslModelToCSharp.Application;
+using FileToDslModel;
+using FileToDslModel.Lexer;
+using FileToDslModel.ParseAutomat;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DslModelToCSharp.Tests
+{
+        [TestClass]
+        public class DependencyInjectionWriterTests : TestBase
+        {
+            [TestMethod]
+            public void Write()
+            {
+                var hookResultBuilder = new DependencyInjectionWriter(ApplicationNameSpace);
+
+                using (var reader = new StreamReader("Schema.wsb"))
+                {
+                    var content = reader.ReadToEnd();
+                    var domainTree = new DslParser(new Tokenizer(), new Parser()).Parse(content);
+
+                    hookResultBuilder.Write(domainTree.Classes, domainTree.SynchronousDomainHooks, "Application/Base/");
+                }
+
+                Assert.AreEqual(File.ReadAllText("../../../ApplicationExpected/Generated/Base/GeneratedDependencies.g.cs"),
+                    File.ReadAllText("Application/Base/GeneratedDependencies.g.cs"));
+            }
+        }
+}
