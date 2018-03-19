b9e563d8015d9fc59ef17ac7aa883e5df5be235a
Simon Heiss <simonheiss87@gmail.com>
2018-03-19 18:41:56 +0100

    removed lineendings again

--- DslModelToCSharp/PrivateSetPropertyHackCleaner.cs
+++ DslModelToCSharp/PrivateSetPropertyHackCleaner.cs
@@ -12,7 +12,7 @@ namespace DslModelToCSharp
                 foreach (var file in Directory.GetFiles(currentDir))
                 {
                     var readAllText = File.ReadAllText(file);
-                    var replace = readAllText.Replace("NewHackGuid302315ed-3a05-4992-9f76-4cf075cde515;", "").Replace("\\r\\n", "\\n");
+                    var replace = readAllText.Replace("NewHackGuid302315ed-3a05-4992-9f76-4cf075cde515;", "");
                     File.WriteAllText(file, replace);
                 }
                 ReplaceHackPropertyNames(currentDir);
