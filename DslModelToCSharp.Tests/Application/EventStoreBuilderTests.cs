14f2c4d9900a8dfff99176f53fcb3ab7af9324ae
Simon Heiss <simonheiss87@gmail.com>
2018-03-20 20:52:33 +0100

    regex reaplace

--- DslModelToCSharp.Tests/Application/EventStoreBuilderTests.cs
+++ DslModelToCSharp.Tests/Application/EventStoreBuilderTests.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using DslModel.Application;
 using DslModelToCSharp.Application;
 using FileToDslModel;
@@ -27,8 +29,8 @@ namespace DslModelToCSharp.Tests.Application
 
             new PrivateSetPropertyHackCleaner().ReplaceHackPropertyNames(ApplicationBasePath);
 
-            Assert.AreEqual(File.ReadAllText("../../../ApplicationExpected/Generated/Base/EventStore.g.cs").Replace("\\n", ""),
-                File.ReadAllText("Application/Base/EventStore.g.cs").Replace("\\r\\n", ""));
+            Assert.AreEqual(Regex.Replace(File.ReadAllText("../../../ApplicationExpected/Generated/Base/EventStore.g.cs"), @"\s+", String.Empty),
+                Regex.Replace(File.ReadAllText("Application/Base/EventStore.g.cs"),  @"\s+", String.Empty));
         }
     }
 }
