84439524df39c55b7ce8d7593cd0036138634673
Lauchi <simonheiss87@gmail.com>
2018-03-22 22:06:27 +0100

    fixed test

--- DslModelToCSharp.Tests/ApplicationExpected/Generated/Users/UserCommandHandler.g.cs
+++ DslModelToCSharp.Tests/ApplicationExpected/Generated/Users/UserCommandHandler.g.cs
@@ -113,9 +113,9 @@ namespace Application.Users
             {
                 var errorList = new List<string>();
                 var NewPost = await PostRepository.GetPost(apiCommand.NewPostId);
-                if (NewPost == null) errorList.Add($"Could not find @Load Post with ID: {id}");
+                if (NewPost == null) errorList.Add($"Could not find Post for {nameof(apiCommand.NewPostId)} with ID: {id}");
                 var PostToDelete = await PostRepository.GetPost(apiCommand.PostToDeleteId);
-                if (PostToDelete == null) errorList.Add($"Could not find @Load Post with ID: {id}");
+                if (PostToDelete == null) errorList.Add($"Could not find Post for {nameof(apiCommand.PostToDeleteId)} with ID: {id}");
                 if (errorList.Count > 0) return new NotFoundObjectResult(errorList);
                 var command = new UserAddPostCommand(NewPost, PostToDelete);
                 var validationResult = entity.AddPost(command);
