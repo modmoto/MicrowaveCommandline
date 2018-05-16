dc13cee12b264cf4f23b85e0653a46cbb72550c4
Simon Heiss <simonheiss87@gmail.com>
2018-05-16 23:21:35 +0200

    delete unused class, added a test for parser

--- Microwave.LanguageParser.Tests/ParseAutomat/Members/LoadFromDbMethodParamTests.cs
+++ Microwave.LanguageParser.Tests/ParseAutomat/Members/LoadFromDbMethodParamTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microwave.LanguageParser.Lexer;
 using Microwave.LanguageParser.ParseAutomat;
@@ -41,42 +42,82 @@ namespace Microwave.LanguageParser.Tests.ParseAutomat.Members
         }
 
         [TestMethod]
-                public void LoadFromDbParam_SecondParameterIsNotALoadParameter()
-                {
-                    var tokens = new Collection<DslToken>
-                    {
-                        new DslToken(TokenType.DomainClass, "DomainClass", 1),
-                        new DslToken(TokenType.Value, "User", 1),
-                        new DslToken(TokenType.ObjectBracketOpen, "{", 1),
-                        new DslToken(TokenType.Value, "AddPost", 2),
-                        new DslToken(TokenType.ParameterBracketOpen, "(", 2),
-                        new DslToken(TokenType.Value, "NewPost", 2),
-                        new DslToken(TokenType.TypeDefSeparator, ":", 2),
-                        new DslToken(TokenType.LoadToken, "@Load", 2),
-                        new DslToken(TokenType.Value, "Post", 2),
-                        new DslToken(TokenType.ParamSeparator, ",", 2),
-                        new DslToken(TokenType.Value, "SecondPost", 2),
-                        new DslToken(TokenType.TypeDefSeparator, ":", 2),
-                        new DslToken(TokenType.Value, "Post", 2),
-                        new DslToken(TokenType.ParameterBracketClose, ")", 2),
-                        new DslToken(TokenType.TypeDefSeparator, ":", 2),
-                        new DslToken(TokenType.ObjectBracketOpen, "{", 2),
-                        new DslToken(TokenType.ObjectBracketClose, "}", 2),
-                        new DslToken(TokenType.ObjectBracketClose, "}", 3)
-                    };
+        public void LoadFromDbParam_SecondParameterIsNotALoadParameter()
+        {
+            var tokens = new Collection<DslToken>
+            {
+                new DslToken(TokenType.DomainClass, "DomainClass", 1),
+                new DslToken(TokenType.Value, "User", 1),
+                new DslToken(TokenType.ObjectBracketOpen, "{", 1),
+                new DslToken(TokenType.Value, "AddPost", 2),
+                new DslToken(TokenType.ParameterBracketOpen, "(", 2),
+                new DslToken(TokenType.Value, "NewPost", 2),
+                new DslToken(TokenType.TypeDefSeparator, ":", 2),
+                new DslToken(TokenType.LoadToken, "@Load", 2),
+                new DslToken(TokenType.Value, "Post", 2),
+                new DslToken(TokenType.ParamSeparator, ",", 2),
+                new DslToken(TokenType.Value, "SecondPost", 2),
+                new DslToken(TokenType.TypeDefSeparator, ":", 2),
+                new DslToken(TokenType.Value, "Post", 2),
+                new DslToken(TokenType.ParameterBracketClose, ")", 2),
+                new DslToken(TokenType.TypeDefSeparator, ":", 2),
+                new DslToken(TokenType.ObjectBracketOpen, "{", 2),
+                new DslToken(TokenType.ObjectBracketClose, "}", 2),
+                new DslToken(TokenType.ObjectBracketClose, "}", 3)
+            };
 
-                    var parser = new MicrowaveLanguageParser();
-                    var domainTree = parser.Parse(tokens);
+            var parser = new MicrowaveLanguageParser();
+            var domainTree = parser.Parse(tokens);
 
-                    Assert.AreEqual(1, domainTree.Classes[0].Methods.Count);
-                    Assert.AreEqual(0, domainTree.Classes[0].Properties.Count);
-                    Assert.AreEqual("AddPost", domainTree.Classes[0].Methods[0].Name);
-                    Assert.AreEqual(1, domainTree.Classes[0].Methods[0].LoadParameters.Count);
-                    Assert.AreEqual(1, domainTree.Classes[0].Methods[0].Parameters.Count);
-                    Assert.AreEqual("SecondPost", domainTree.Classes[0].Methods[0].Parameters[0].Name);
-                    Assert.AreEqual("NewPost", domainTree.Classes[0].Methods[0].LoadParameters[0].Name);
-                    Assert.AreEqual("Post", domainTree.Classes[0].Methods[0].Parameters[0].Type);
-                    Assert.AreEqual("Post", domainTree.Classes[0].Methods[0].LoadParameters[0].Type);
-                }
+            Assert.AreEqual(1, domainTree.Classes[0].Methods.Count);
+            Assert.AreEqual(0, domainTree.Classes[0].Properties.Count);
+            Assert.AreEqual("AddPost", domainTree.Classes[0].Methods[0].Name);
+            Assert.AreEqual(1, domainTree.Classes[0].Methods[0].LoadParameters.Count);
+            Assert.AreEqual(1, domainTree.Classes[0].Methods[0].Parameters.Count);
+            Assert.AreEqual("SecondPost", domainTree.Classes[0].Methods[0].Parameters[0].Name);
+            Assert.AreEqual("NewPost", domainTree.Classes[0].Methods[0].LoadParameters[0].Name);
+            Assert.AreEqual("Post", domainTree.Classes[0].Methods[0].Parameters[0].Type);
+            Assert.AreEqual("Post", domainTree.Classes[0].Methods[0].LoadParameters[0].Type);
+        }
+
+        [TestMethod]
+        public void LoadFromDbParam_Error_DoubleLoad()
+        {
+            var tokens = new Collection<DslToken>
+            {
+                new DslToken(TokenType.DomainClass, "DomainClass", 1),
+                new DslToken(TokenType.Value, "User", 1),
+                new DslToken(TokenType.ObjectBracketOpen, "{", 1),
+                new DslToken(TokenType.Value, "AddPost", 2),
+                new DslToken(TokenType.ParameterBracketOpen, "(", 2),
+                new DslToken(TokenType.Value, "NewPost", 2),
+                new DslToken(TokenType.TypeDefSeparator, ":", 2),
+                new DslToken(TokenType.LoadToken, "@Load", 2),
+                new DslToken(TokenType.LoadToken, "@Load", 2),
+                new DslToken(TokenType.Value, "Post", 2),
+                new DslToken(TokenType.ParamSeparator, ",", 2),
+                new DslToken(TokenType.Value, "SecondPost", 2),
+                new DslToken(TokenType.TypeDefSeparator, ":", 2),
+                new DslToken(TokenType.Value, "Post", 2),
+                new DslToken(TokenType.ParameterBracketClose, ")", 2),
+                new DslToken(TokenType.TypeDefSeparator, ":", 2),
+                new DslToken(TokenType.ObjectBracketOpen, "{", 2),
+                new DslToken(TokenType.ObjectBracketClose, "}", 2),
+                new DslToken(TokenType.ObjectBracketClose, "}", 3)
+            };
+
+            var parser = new MicrowaveLanguageParser();
+            try
+            {
+                parser.Parse(tokens);
+            }
+            catch (NoTransitionException e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+
+            Assert.Fail();
+        }
     }
 }
